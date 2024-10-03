3f9464cd9dc9649a6da8d3d0fe4470bfb3b9f93e
Damien29990 <90838963+Damien29990@users.noreply.github.com>
2024-10-03 13:24:01 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+import { WORDS } from "./word.js";
+
+let answer = WORDS[Math.floor(Math.random() * WORDS.length)];
+console.log(answer);
+
+console.log(answer);
+console.log(answer);
+console.log(answer);
